3db929ff50c73bddd72b8dd076e1f1eaa1386da0
Matej Ugrin <matej.ugrin@ingka.com>
2024-07-20 23:16:11 +0200

    Modify query sample

--- sandbox.sql
+++ sandbox.sql
@@ -1,3 +1,3 @@
 -- Sample queries
--- duckdb
-select count(*) cnt, min(event_time) min_time from read_parquet('/Users/matej/development/personal/duckdb-sandbox/results/single_event.parquet')
+-- duckdb - run the query in the interactive shell
+select count(*) cnt, min(event_time) min_time from read_parquet('results/single_event.parquet')
